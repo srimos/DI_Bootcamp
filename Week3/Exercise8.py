d25e5cba644c093b077402eb54885a78e8cefbb2
srimos <shaun.rimos@gmail.com>
2025-05-17 09:11:27 +0400

    finished Python Exercises

---
+++ Week3/Exercise8.py
@@ -0,0 +1,5 @@
+x=input("Your name: ")
+if x=="Shaun":
+    print("Oh wow! We have the same name!!!")
+elif x!="Shaun":
+    print("Cool, nice to meet you!")
---
+++ Week3/Exercise9.py
@@ -0,0 +1,5 @@
+x=input("Please state your height(cm): ")
+if int(x)>=145:
+    print("You are tall enough to ride.")
+elif int(x)<145:
+    print("You need to greow some more before you can ride.")
