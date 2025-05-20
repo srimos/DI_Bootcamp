628074634a0110b1a82a9e73717738fb929ff0ea
srimos <shaun.rimos@gmail.com>
2025-05-20 17:52:55 +0400

    Week3 Exercises

---
+++ Week3/Day4/ExercisesXP/Exercise1.py
@@ -0,0 +1,11 @@
+my_fav_numbers={1,2,3,4,5}
+print(my_fav_numbers)
+my_fav_numbers.add(10)
+my_fav_numbers.add(100)
+print(my_fav_numbers)
+my_fav_numbers.remove(100)
+print(my_fav_numbers)
+friend_fav_numbers={6,7,8,9,50}
+print(friend_fav_numbers)
+our_fav_numbers =set(list(my_fav_numbers) + list(friend_fav_numbers))
+print(our_fav_numbers)
---
+++ Week3/Day4/ExercisesXP/Exercise2.py
@@ -0,0 +1,11 @@
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+a,b,c,d,e=my_tuple
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+my_tups=a,b,c,d,e
+my_tups.__add__(10)
+print(my_tups)
