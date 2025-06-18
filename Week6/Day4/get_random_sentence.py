1f1c1d80d226c442ffabdad9f05718a5457d50eb
srimos <shaun.rimos@gmail.com>
2025-06-19 00:22:00 +0400

    Week6 Day4 ExercisesXP

---
+++ Week6/Day4/get_random_sentence.py
@@ -0,0 +1,14 @@
+from random import choice
+from get_words_from_file import get_words_from_file
+
+def get_random_sentence(sentence_length):
+    get_words_from_file()
+    random_word_list=[]
+    print(words)
+    print(sentence_length)
+    # for i in sentence_length:
+    #     random_word=random.choice(words)
+    #     random_word_list.append(random_word)
+    # random_sentence=" ".join(random_word_list)
+
+    # return random_sentence
