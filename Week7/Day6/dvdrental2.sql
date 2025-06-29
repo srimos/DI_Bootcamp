1bb66d4b3b947b2db1d553000a33e1ef003eaf22
srimos <shaun.rimos@gmail.com>
2025-06-30 00:09:10 +0400

    Week7 Day6 ExerciseXP

--- Week7/Day6/dvdrental2.sql
+++ Week7/Day6/dvdrental2.sql
@@ -9,11 +9,11 @@ select film.title, film.description, language.name as language_name from film ri
 
 -- 4. Create a new table called new_film with the following columns : id, name. Add some new films to the table.
 create table new_film (
-new_file_id serial primary key,
-new_file_name varchar (50) not null
+new_film_id serial primary key,
+new_film_name varchar (50) not null
 );
 
-insert into new_film (new_file_id, new_file_name)
+insert into new_film (new_film_id, new_film_name)
 values (1,'One More Time'),
 (2,'It Takes Two to Tango'),
 (3,'Three Blind Mice'),
@@ -31,32 +31,57 @@ values (1,'One More Time'),
 -- last_update – when the review was last updated.
 create table customer_review (
 review_id serial primary key not null,
-new_film_id smallint not null,           
+film_id smallint not null references new_film (new_film_id) on delete cascade,          
 language_id smallint not null,
 title varchar (50) not null,
 score smallint not null,
 review_text text,
-last_update date;
-)
+last_update date
+);
 
 -- 6. Add 2 movie reviews. Make sure you link them to valid objects in the other tables.
+insert into customer_review (review_id,film_id,language_id,title,score,review_text,last_update)
+values (1,1,1,'Would Definitely Watch One More Time!',9,'This movie was one of the best I have ever seen!',current_date),
+(2,4,1,'Fantastic!',8,'Finally, a good Fantastic Four movie!',current_date);
 
 -- 7. Delete a film that has a review from the new_film table, what happens to the customer_review table?
+delete from new_film where new_film_id=4;
 
 -- Use UPDATE to change the language of some films. Make sure that you use valid languages.
+update film set language_id=2 where title ilike '%italian%';
 
 -- Which foreign keys (references) are defined for the customer table? How does this affect the way in which we INSERT into the customer table?
+store_id and address_id;
 
 -- We created a new table called customer_review. Drop this table. Is this an easy step, or does it need extra checking?
+drop table customer_review;
 
 -- Find out how many rentals are still outstanding (ie. have not been returned to the store yet).
+select count(*) from rental where return_date is null;
 
 -- Find the 30 most expensive movies which are outstanding (ie. have not been returned to the store yet)
+select film.title, film.rental_rate
+from rental
+inner join inventory on rental.inventory_id = inventory.inventory_id
+inner join film on inventory.film_id = film.film_id
+where return_date is null 
+order by rental_rate desc
+limit 30;
 
 -- Your friend is at the store, and decides to rent a movie. He knows he wants to see 4 movies, but he can’t remember their names. Can you help him find which movies he wants to rent?
 -- The 1st film : The film is about a sumo wrestler, and one of the actors is Penelope Monroe.
+select film.title, film.description, actor.first_name, actor.last_name 
+from film 
+inner join film_actor on film_actor.film_id = film.film_id
+inner join actor on actor.actor_id = film_actor.actor_id
+where description ilike '%sumo%' and first_name='Penelope' and last_name='Monroe';
 
 -- The 2nd film : A short documentary (less than 1 hour long), rated “R”.
+select film.title, category.name, film.length, film.rating
+from film 
+inner join film_category on film_category.film_id = film.film_id
+inner join category on film_category.category_id = category.category_id
+where category.name ilike '%documentary%' and length < 60 and rating = 'R';
 
 -- The 3rd film : A film that his friend Matthew Mahan rented. He paid over $4.00 for the rental, and he returned it between the 28th of July and the 1st of August, 2005.
 
