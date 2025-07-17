f3fd13012c158ec871e6743547122f882894830e
srimos <shaun.rimos@gmail.com>
2025-07-17 21:01:35 +0400

    Week12

--- Week12/animal-info/animals/info/urls.py
+++ Week12/animal-info/animals/info/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('animals', views.display_all_animals, name='animals'),
     path('families', views.display_all_families, name='families'),
     path('animal/<int:animal_id>:', views.display_one_animal, name='animal'),
-    path('family/<int:family_id>', views.display_animal_per_family, name='family'),
+    path('animal_in_family/<int:family_id>', views.display_animal_per_family, name='family'),
 ]
