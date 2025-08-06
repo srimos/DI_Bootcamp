7288258ddf2dac2f524b60cd7692de9e1c3de085
srimos <shaun.rimos@gmail.com>
2025-08-07 01:00:12 +0800

    Day2

--- Week13/bike_store/vehicle/serialisers.py
+++
@@ -1,8 +0,0 @@
-from .models import Vehicle
-from rest_framework import serializers
-
-class VechileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = '__all__' 
-        
