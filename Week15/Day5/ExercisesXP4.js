4739f11541696a07ed11d893bdba9489f5e56957
srimos <shaun.rimos@gmail.com>
2025-08-19 13:06:10 +0400

    Week15 Day5 ExercisesXP

--- Week15/Day5/ExercisesXP4.js
+++ Week15/Day5/ExercisesXP4.js
@@ -9,7 +9,7 @@ let allBooks=[
     title: "Artemis Fowl",
     author: "Eoin Colfer",
     image: "https://www.artemis-fowl.com/wp-content/uploads/2015/01/artemis-fowl-book-1-cover.png",
-    alreadyRead: true
+    alreadyRead: false
     }
 ];
 console.log(allBooks)
@@ -18,49 +18,51 @@ let tableNew = document.createElement('table');
 div.appendChild(tableNew);
 let theadNew = document.createElement('thead');
 tableNew.appendChild(theadNew);
-let trNew1 = document.createElement('tr');
-theadNew.appendChild(trNew1);
+let theadrNew = document.createElement('tr');
+theadNew.appendChild(theadrNew);
 for (i=0;i<2;i++) {
     let thNew = document.createElement('th');
-    trNew1.appendChild(thNew);
+    theadrNew.appendChild(thNew);
 }
-
 document.getElementsByTagName("th")[0].textContent = "Title and Author"
 document.getElementsByTagName("th")[1].textContent = "Image"
 
 let tbodyNew = document.createElement('tbody');
 tableNew.appendChild(tbodyNew);
 
-let trNew2 = document.createElement('tr');
-tbodyNew.appendChild(trNew2);
 for (i=0;i<2;i++) {
-    let tdNew = document.createElement('td');
-    trNew2.appendChild(tdNew);
-}
-document.getElementsByTagName("td")[0].textContent = `${allBooks[0].title} written by ${allBooks[0].author}`;
-let imageNew1=document.createElement('img');
-imageNew1.src=allBooks[0].image;
-imageNew1.setAttribute('style','width:100px;');
-document.getElementsByTagName("td")[1].appendChild(imageNew1)
-// document.getElementsByTagName("td")[1].src = allBooks[0].image;
-if (allBooks[0].alreadyRead == true) {
-    document.getElementsByTagName("tr")[1].setAttribute('style','color:red');
+    let tbodyrNew = document.createElement('tr');
+    tbodyNew.appendChild(tbodyrNew);
+    for (j=0;j<2;j++) {
+        let tdNew = document.createElement('td');
+        tbodyrNew.appendChild(tdNew);
+    }
+    document.getElementsByTagName("tr")[i+1].firstChild.textContent = `${allBooks[i].title} written by ${allBooks[i].author}`;
+    let imageNew=document.createElement('img');
+    imageNew.src=allBooks[i].image;
+    imageNew.setAttribute('style','width:100px;');
+    document.getElementsByTagName("tr")[i+1].firstChild.nextSibling.appendChild(imageNew)
+    if (allBooks[i].alreadyRead == true) {
+    document.getElementsByTagName("tr")[i+1].setAttribute('style','color:red');
+    }
 }
 
-let trNew3 = document.createElement('tr');
-tbodyNew.appendChild(trNew3);
-for (i=0;i<2;i++) {
-    let tdNew = document.createElement('td');
-    trNew3.appendChild(tdNew);
-}
-document.getElementsByTagName("td")[2].textContent = `${allBooks[1].title} written by ${allBooks[1].author}`;
-let imageNew2=document.createElement('img');
-imageNew2.src=allBooks[1].image;
-imageNew2.setAttribute('style','width:100px;');
-document.getElementsByTagName("td")[3].appendChild(imageNew2)
-// document.getElementsByTagName("td")[1].src = allBooks[0].image;
-if (allBooks[1].alreadyRead == true) {
-    document.getElementsByTagName("tr")[2].setAttribute('style','color:red');
-}
+
+
+// let trNew3 = document.createElement('tr');
+// tbodyNew.appendChild(trNew3);
+// for (i=0;i<2;i++) {
+//     let tdNew = document.createElement('td');
+//     trNew3.appendChild(tdNew);
+// }
+// document.getElementsByTagName("td")[2].textContent = `${allBooks[1].title} written by ${allBooks[1].author}`;
+// let imageNew2=document.createElement('img');
+// imageNew2.src=allBooks[1].image;
+// imageNew2.setAttribute('style','width:100px;');
+// document.getElementsByTagName("td")[3].appendChild(imageNew2)
+// // document.getElementsByTagName("td")[1].src = allBooks[0].image;
+// if (allBooks[1].alreadyRead == true) {
+//     document.getElementsByTagName("tr")[2].setAttribute('style','color:red');
+// }
 
 console.log(div)
