ebc0f50b8f912d1a8e388b6e3b58337b610fb443
srimos <shaun.rimos@gmail.com>
2025-08-20 14:16:43 +0400

    Week15 Day2 DailyChallenge

--- Week15/Day2/DailyChallenge.js
+++ Week15/Day2/DailyChallenge.js
@@ -1,25 +1,34 @@
 function wordsInTheStars() {
-    input=prompt("Give a sentence with each word separated by a comma ',' and no spaces")
-    array=input.split(",")
-    console.log(array)
-    longestWord=0
+    let input=prompt("Give a sentence with each word separated by a comma ',' and no spaces")
+    let array=input.split(",")
+    // console.log(array)
+    let longestWord=0
     for (word of array) {
         wordLength=word.length
         if (wordLength>longestWord) {
             longestWord=wordLength
         }
     }
-    console.log(longestWord)
-    arrLength=longestWord+4
-    arrHeight=array.length+2
-    let arr = Array.from({ length: longestWord }, () => Array(6).fill(""));
-    for (i=0;i<longestWord;i++) {
-        arr[0][i]="*"
+    // console.log(longestWord)
+    let arrLength=longestWord+4
+    // console.log(arrLength)
+    let arrHeight=array.length+4
+    // console.log(arrHeight)
+    let arr = Array.from({ length: arrHeight }, () => Array(arrLength).fill("*"));
+    for (i=1;i<arrHeight-1;i++) {
+        for (j=1;j<arrLength-1;j++) {
+            arr[i][j]=" "
+        }
     }
-    for (i=0;i<longestWord;i++) {
-        arr[1][i]="*"
-
+    for (i=0;i<array.length;i++) {
+        for (j=0;j<array[i].length;j++) {
+            arr[i+2][j+2]=array[i][j]
+        }
     }
+    arr.forEach(row => {
+    console.log(row.join(" "));
+    });
+    // console.log(arr)
 }
 
 wordsInTheStars()
