4930ceac05f08e2d2d00012d5560f39d4d525dcb
srimos <shaun.rimos@gmail.com>
2025-08-27 13:49:27 +0400

    Week3Day6

--- Week3/Day6/ExercisesXP/Exercise2.py
+++ Week3/Day6/ExercisesXP/Exercise2.py
@@ -5,12 +5,19 @@ family={
     'summer':8
 }
 
-cost=[]
-
-print(family)
+cost={}
+total=0
 
 for x,y in family.items():
-    if x <=12:
+    if y<3:
+        cost[x]=0
+    elif y>=3 and y<12:
+        cost[x]=10
+    elif y>=12:
+        cost[x]=15
+    print("Ticket price for",x,":",cost[x])
+    total=total+cost[x]
 
+print("Total cost:",total)
 
 print('Finished!')
