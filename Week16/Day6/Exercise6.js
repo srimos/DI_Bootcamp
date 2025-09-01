785a2e8445b23480fb52b75f6509ee2ad5d8120a
srimos <shaun.rimos@gmail.com>
2025-09-01 14:01:15 +0400

    Week16 Day 6

--- Week16/Day6/Exercise6.js
+++ Week16/Day6/Exercise6.js
@@ -2,16 +2,21 @@ const details = ["+200", "-100", "+146", "+167", "-2900"]
 vat=1.17
 
 let transactions=(details)=>{
-        for (let detail in details){
-                detail=parseInt(detail)*vat
-}}
-console.log(transactions)
-
-let bankAmount=5000
-console.log(bankAmount)
-
-let expenses=(bankAmount,details)=>{
-        for (let detail in details){
-                bankAmount=bankAmount+detail
+        let result=[]
+        for (let detail of details){
+                result.push(parseInt(detail)*vat)
         }
+        return result
 }
+
+var bankAmount=5000
+
+let expenses=(bankAmount,transactions)=>{
+        for (let t of transactions){
+                bankAmount=bankAmount+t
+        }
+        return bankAmount
+}
+
+let newbankAmount=expenses(bankAmount,transactions(details));
+console.log(newbankAmount)
