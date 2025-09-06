41919df5c387c0b67bb373cdf39ecacab796798b
srimos <shaun.rimos@gmail.com>
2025-09-07 00:30:34 +0400

    Week12 Day2

--- Week12/Day2/books-info/books_project/books/models.py
+++ Week12/Day2/books-info/books_project/books/models.py
@@ -1,21 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator,MaxValueValidator,MinLengthValidator
 
 class Book(models.Model):
     title = models.CharField(max_length=200, blank=False)
     author = models.CharField(max_length=100, blank=False)
     published_date = models.DateField(blank=False)
     description = models.TextField(blank=False)
-    page_count = models.IntegerField(min_value=0)
+    page_count = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
     categories = models.CharField(max_length=100, blank=False)
     thumbnail_url = models.URLField()
+
+    def __str__(self):
+        return self.title
 
 class BookReview(models.Model):
     book = models.ForeignKey(Book,
         blank=False, 
         on_delete=models.CASCADE,
-        primary_key=True,
     )
-    user = models.ForeignKey(blank=False)
-    rating = models.IntegerField()
-    rating = models.IntegerField(min_value=1, max_value=5)
-    review_text = models.TextField(min_length=10)
+    user = models.ForeignKey(User,
+        blank=False,
+        on_delete=models.CASCADE,
+    )
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    review_text = models.TextField(
+        validators=[MinLengthValidator(10)]
+    )
--- Week12/Day2/books-info/books_project/books/views.py
+++ Week12/Day2/books-info/books_project/books/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Book, BookReview 
+from django.http import JsonResponse
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    data = []
+
+    for book in books:
+        data.append({
+            "title": book.title,
+        })
+
+    return JsonResponse(data, safe=False)
+
+def book_detail(request,id):
+    book=Book.objects.get(id=id)
+    data = {
+        "id": book.id,
+        "title": book.title,
+        "author": book.author,
+        "published_date": book.published_date,
+        "description": book.description,
+        "page_count": book.page_count,
+        "categories": book.categories,
+        "thumbnail_url": book.thumbnail_url
+    }    
+
+    return JsonResponse(data)
