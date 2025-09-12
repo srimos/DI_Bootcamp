3e0e37a55c716da11e67869357c569b06591eb93
srimos <shaun.rimos@gmail.com>
2025-09-12 13:45:33 +0400

    Week17 Day2

--- Week17/Day2/DailyChallenge/DailyChallenge.js
+++ Week17/Day2/DailyChallenge/DailyChallenge.js
@@ -19,4 +19,15 @@ const displayGroceries = (groceries) => {
 
 displayGroceries(groceries)
 
-const cloneGroceries = ()
+const cloneGroceries = (client) => {
+    user = client
+    client = "Betty"
+    console.log(user)
+    shopping = groceries
+    groceries.totalPrice = "35$"
+    groceries.other.paid = false
+    console.log(shopping)
+}
+// No modification to user variable because "Betty" is a primitive value and it is pass by value.
+// The modification will be reflected in the shopping variable because totalPrice and paid are in the groceries object which is not a primitive value and it is pass by reference.
+cloneGroceries (client)
