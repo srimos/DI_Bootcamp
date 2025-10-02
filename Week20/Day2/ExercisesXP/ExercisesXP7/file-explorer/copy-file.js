f2a1278ed762ed56867f898c942c4fdf1121759f
srimos <shaun.rimos@gmail.com>
2025-10-02 10:13:33 +0400

    Week20 Day2 ExercisesXP

---
+++ Week20/Day2/ExercisesXP/ExercisesXP7/file-explorer/copy-file.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+
+async function readFile(file) {
+    try {
+        const data = await fs.readFile(file, "utf-8")
+        return data;
+        } catch (err) {
+            console.error('Error reading file: ', err);
+        }
+}
+
+async function writeFile(file,content) {
+    try {
+        await fs.writeFile(file,content);
+        console.log("File written successfully");
+        } catch (err) {
+            console.error('Error writing file: ', err);
+        }
+}
+
+async function readWrite (){
+    const content = await readFile("source.txt");
+    if (content){
+        writeFile("destination.txt",content);
+    }
+}
+
+readWrite()
