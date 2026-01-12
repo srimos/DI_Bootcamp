8662fbdf53f28e8cc4c82c09f747e876d94cbd96
srimos <shaun.rimos@gmail.com>
2026-01-12 22:05:23 +0400

    fix build.sh

--- Week24/Hackathon2/recipe_project/backend/recipe_project/settings.py
+++ Week24/Hackathon2/recipe_project/backend/recipe_project/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hg_^rng*57lph5go-q4y2xy^l7=53tg+1@n&q_6w=t_)7e6_t9'
+# SECRET_KEY = 'django-insecure-hg_^rng*57lph5go-q4y2xy^l7=53tg+1@n&q_6w=t_)7e6_t9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -53,6 +54,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,11 +100,16 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipes',
-        'USER': 'neondb_owner',
-        'PASSWORD': 'npg_Jbx8Aq6cIBLu',
-        'HOST': 'ep-quiet-shape-a1amc42u-pooler.ap-southeast-1.aws.neon.tech',
-        'PORT': '5432'
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
+        # 'NAME': 'recipes',
+        # 'USER': 'neondb_owner',
+        # 'PASSWORD': 'npg_Jbx8Aq6cIBLu',
+        # 'HOST': 'ep-quiet-shape-a1amc42u-pooler.ap-southeast-1.aws.neon.tech',
+        # 'PORT': '5432'
     }
 }
 
@@ -142,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
