a64e6ee1fc1845ff8d4369babd8d19e444e75e58
srimos <shaun.rimos@gmail.com>
2026-01-12 23:40:35 +0400

    frontend connected

--- Week24/Hackathon2/recipe_project/backend/recipe_project/settings.py
+++ Week24/Hackathon2/recipe_project/backend/recipe_project/settings.py
@@ -67,7 +67,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://recipe-project-zflj.onrender.com/",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
