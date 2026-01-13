afba688ace5f17ed4efb3d7d27a5afecb41dabd8
srimos <shaun.rimos@gmail.com>
2026-01-13 20:56:07 +0400

    fixed

--- Week24/Hackathon2/recipe_project/frontend/src/App.jsx
+++ Week24/Hackathon2/recipe_project/frontend/src/App.jsx
@@ -15,9 +15,9 @@ import { useEffect } from "react";
 function App() {
 
   useEffect(() => {
-    console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+    console.log("REACT_APP_API_URL =", process.env.REACT_APP_API_URL);
   }, []);
-  
+
   return (
     <AuthProvider>
       <Router>
--- Week24/Hackathon2/recipe_project/frontend/src/api.js
+++ Week24/Hackathon2/recipe_project/frontend/src/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
-if (!BASE_URL) console.error("VITE_API_URL is undefined!");
+console.log("API BASE:", BASE_URL);
 
 const api = axios.create({
   baseURL: BASE_URL + "/api/",
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
