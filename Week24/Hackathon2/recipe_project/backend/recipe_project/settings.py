66fb60354ab13be702bf8434ac2f3747ffa8c5cb
srimos <shaun.rimos@gmail.com>
2026-01-13 08:12:19 +0400

    backend

--- Week24/Hackathon2/recipe_project/backend/recipe_project/settings.py
+++ Week24/Hackathon2/recipe_project/backend/recipe_project/settings.py
@@ -101,12 +101,14 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DATABASE_NAME"),
-        "USER": os.environ.get("DATABASE_USER"),
-        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("DATABASE_PORT", "5432"),
-        "OPTIONS": {"sslmode": "require"},
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": os.environ["DATABASE_HOST"],
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        },
         # 'NAME': 'recipes',
         # 'USER': 'neondb_owner',
         # 'PASSWORD': 'npg_Jbx8Aq6cIBLu',
