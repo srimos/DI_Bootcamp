0f8872d547178f79f10b8298260f4a95fd0193db
srimos <shaun.rimos@gmail.com>
2026-01-13 14:26:26 +0400

    frontend

--- Week24/Hackathon2/recipe_project/frontend/src/api.js
+++ Week24/Hackathon2/recipe_project/frontend/src/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
+if (!BASE_URL) console.error("VITE_API_URL is undefined!");
+
 const api = axios.create({
   baseURL: BASE_URL + "/api/",
 });
